9852cdfa48fabf4762eb7f492ddd86d58e31412a
unknown <sthutika@USHYDSTHUTIKA4.us.deloitte.com>
2015-09-24 15:26:39 +0530

    testsing heroku

---
+++ src/main/java/com/cons/SpringHerokuApplication.java
@@ -0,0 +1,13 @@
+package com.cons;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringHerokuApplication {
+
+    public static void main(String[] args) {
+    	System.out.println("THIS IS SUDHIR");
+        SpringApplication.run(SpringHerokuApplication.class, args);
+    }
+}
